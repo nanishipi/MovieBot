7bbd5340560dbdf63b152a3110591e23fb68f64e
nanishipi <gonalo78@gmail.com>
2023-05-20 22:22:35 +0100

    hid token for disc bot + some changes

--- bot.py
+++ bot.py
@@ -1,12 +1,41 @@
+from dotenv import load_dotenv
 import discord
+import os
 import responses
+
+load_dotenv()
 
 async def send_message(message, user_message, is_private):
     try:
-        response = responses.handle_response(user_message)
+        response = responses.handle_response(user_message, message.author.id)
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e: 
         print(e)
 
 def run_discord_bot():
-    
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!');
+        
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        usermame = str(message.author)
+        userMessage = str(message.content)
+        channel = str(message.channel)
+        
+        print(f'{usermame} said: "{userMessage}" ({channel})')
+        
+        if userMessage[0] == '?':
+            userMessage = userMessage[1:]
+            await send_message(message, userMessage, is_private=True)
+        else:
+            await send_message(message, userMessage, is_private=False)
+    print(os.getenv('KEY'))
+    client.run(os.getenv('TOKEN'))
--- responses.py
+++ responses.py
@@ -1,14 +1,23 @@
 import random
 
-def handle_response(message) -> str:
+def handle_response(message, messageAuthorId) -> str:
     p_message = message.lower()
+    print(p_message)
+    
+    if "$addmovie" in p_message:
+        return "Filme adicionado na tua cabeça piquene de merd"
 
     if p_message == 'hello':
-        return 'Shut the fuck up'
+        return 'Olá o meu nome é Cristiano Amigo e eu não gosto do pedro porque ele não tem cabelo'
     
     if p_message == 'roll':
         return str(random.randint(1,6))
     
     if p_message == '!help':
         return "`This is a help message that you can modify`"
-
+    
+    if p_message == 'joke':
+        return "A cara do pedro"
+    
+    if messageAuthorId == 171379982046724096:
+        return 'https://tenor.com/view/zain-ssbm-melee-summit-smash-gif-22400831'    
